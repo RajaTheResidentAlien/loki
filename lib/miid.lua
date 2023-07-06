92f0d2b83d4d13f5d612e6baf63846bae929ff19
Raja <45028516+RajaTheResidentAlien@users.noreply.github.com>
2023-07-05 17:53:38 -0700

    Update miid.lua
    penultimate-beta checkin(?)

--- lib/miid.lua
+++ lib/miid.lua
@@ -1,13 +1,13 @@
 --Master preset stuff(MIDI Program Changes)
-
+m=midi.connect(2)
 function mpwrit(nam)                                 --Master preset write
-  local mwritab={} mwritab["SPre"]=params:get("SPre") mwritab["VPre"]=params:get("VPre") 
-  mwritab["SPrePgChD"]=params:get("SPrePgChD") mwritab["SPrePgChU"]=params:get("SPrePgChU")
-  mwritab["SPreActivePgCh"]=params:get("SPreActivePgCh") mwritab["VPrePgChD"]=params:get("VPrePgChD")
-  mwritab["VPrePgChU"]=params:get("VPrePgChU") mwritab["VPreActivePgCh"]=params:get("VPreActivePgCh")
+  local mwritab={} mwritab["SPre"]=params:get("SPre") mwritab["VPre"]=params:get("VPre") --get current 'S'equencer and softcut-'V'oice preset#s
+  mwritab["SPrePgChD"]=params:get("SPrePgChD") mwritab["SPrePgChU"]=params:get("SPrePgChU") --'..PgChD'=Program Change Down,'..PgChU'=Prog Ch Up
+  mwritab["SPreActivePgCh"]=params:get("SPreActivePgCh") mwritab["VPrePgChD"]=params:get("VPrePgChD")--'..PgCh'=Prog Ch applied as toggles
+  mwritab["VPrePgChU"]=params:get("VPrePgChU") mwritab["VPreActivePgCh"]=params:get("VPreActivePgCh")--(i.e.'activate'/'deactivate' function) 
   mwritab["InputPgCh"]=params:get("InputPgCh") mwritab["FreezePgCh"]=params:get("FreezePgCh")
-  mwritab["UIGoPgCh"]=params:get("UIGoPgCh") mwritab["PlayPgCh"]=params:get("PlayPgCh") 
-  mwritab["RestartPgCh"]=params:get("RestartPgCh") mwritab["DelRevPgCh"]=params:get("DelRevPgCh")
+  mwritab["PlayPgCh"]=params:get("PlayPgCh")                                                      
+  mwritab["RestartPgCh"]=params:get("RestartPgCh") mwritab["DelRevPgCh"]=params:get("DelRevPgCh") 
   mwritab["FXVPgCh"]=params:get("FXVPgCh") mwritab["PtchPLPgCh"]=params:get("PtchPLPgCh") 
   mwritab["PtchPRPgCh"]=params:get("PtchPRPgCh") mwritab["PtchPRzPgCh"]=params:get("PtchPRzPgCh")
   for i=1,6 do
@@ -34,7 +34,7 @@ function mpread(nam)                                  --Master preset read
       params:set("SPreActivePgCh",mreadtab["SPreActivePgCh"]) params:set("VPrePgChD",mreadtab["VPrePgChD"]) 
       params:set("VPrePgChU",mreadtab["VPrePgChU"]) params:set("VPreActivePgCh",mreadtab["VPreActivePgCh"])
       params:set("InputPgCh", mreadtab["InputPgCh"]) params:set("FreezePgCh", mreadtab["FreezePgCh"]) 
-      params:set("UIGoPgCh", mreadtab["UIGoPgCh"]) params:set("PlayPgCh", mreadtab["PlayPgCh"])
+      params:set("PlayPgCh", mreadtab["PlayPgCh"])
       params:set("RestartPgCh", mreadtab["RestartPgCh"]) params:set("DelRevPgCh", mreadtab["DelRevPgCh"])
       params:set("FXVPgCh", mreadtab["FXVPgCh"]) params:set("PtchPLPgCh", mreadtab["PtchPLPgCh"])
       params:set("PtchPRPgCh", mreadtab["PtchPRPgCh"]) params:set("PtchPRzPgCh", mreadtab["PtchPRzPgCh"])
@@ -53,32 +53,31 @@ function mpread(nam)                                  --Master preset read
     end
   end
 end
-params:add_number("MPre", "MasterPreset",1,500,1)
-params:set_action("MPre", function(mpre) mpread("M_"..mpre..".lki") end)
+params:add_number("MPre", "MasterPreset",1,500,1) --This File Is Also Where Master Preset Is Handled
+params:set_action("MPre", function(mpre) mpread("M_"..mpre..".lki") end) --load the preset/file
 params:add_number("SPre", "SPreset",1,500,1)
-params:set_action("SPre", function(spre) spread("S_"..spre..".lki") end)
+params:set_action("SPre", function(spre) spread("S_"..spre..".lki") end) --    ""
 params:add_number("VPre", "VPreset",1,500,1)
-params:set_action("VPre", function(vpre) vpread("V_"..vpre..".lki") end)
+params:set_action("VPre", function(vpre) vpread("V_"..vpre..".lki") end) --    ""
 params:add_group("PgCh_Grp","ProgramChange_Group",82)
-params:add_number("SPrePgChD", "SPreProgramChangeDown", 0, 127, 10)
-params:add_number("SPrePgChU", "SPreProgramChangeUp", 0, 127, 11)
+params:add_number("SPrePgChD", "SPreProgramChangeDown", 0, 127, 10) -- S-Page program change down
+params:add_number("SPrePgChU", "SPreProgramChangeUp", 0, 127, 11)  -- S-Page program change up
 params:add_number("SPreActivePgCh", "SPreActiveProgramChange", 0, 127, 15)  --make S-page program changes active
-params:add_number("VPrePgChD", "VPreProgramChangeDown", 0, 127, 20)
+params:add_number("VPrePgChD", "VPreProgramChangeDown", 0, 127, 20)  -- etc...
 params:add_number("VPrePgChU", "VPreProgramChangeUp", 0, 127, 21)
-params:add_number("VPreActivePgCh", "VPreActiveProgramChange", 0, 127, 17)  --make V-page program changes active
-params:add_number("InputPgCh", "InputMonitorProgramChange", 0, 127, 18)
+params:add_number("VPreActivePgCh", "VPreActiveProgramChange", 0, 127, 17)  
+params:add_number("InputPgCh", "InputMonitorProgramChange", 0, 127, 18)     --input-monitor toggle
 params:add_number("FreezePgCh", "FreezeProgramChange", 0, 127, 19)          --freeze tempo-related params
-params:add_number("UIGoPgCh", "UIGoProgramChange", 0, 127, 16)
 params:add_number("PlayPgCh", "PlayProgramChange", 0, 127, 14)              --toggle play on/off
 params:add_number("RestartPgCh", "RestartProgramChange", 0, 127, 30)        --restart transport(w/out off)
 params:add_number("DelRevPgCh", "DelayReverbProgramChange", 0, 127, 4)      --toggle random delay/reverb
 params:add_number("FXVPgCh", "FXVortexProgramChange", 0, 127, 5)            --toggle FX Vortex on/off
-params:add_number("PtchPLPgCh", "PitchPollLeftProgramChange", 0, 127, 6)
-params:add_number("PtchPRPgCh", "PitchPollRightProgramChange", 0, 127, 7)
-params:add_number("PtchPRzPgCh", "PitchPollRezProgramChange", 0, 127, 8)
-for i=1,6 do 
-  params:add_number("V"..i.."PreFreezePgCh", "V"..i.."PreFreezeProgramChange", 0, 127, (10*i)+29)
-  params:add_number("V"..i.."K2PgCh", "V"..i.."K2ProgramChange", 0, 127, (10*i)+30)
+params:add_number("PtchPLPgCh", "PitchPollLeftProgramChange", 0, 127, 6)    --activate left-input pitch-poll
+params:add_number("PtchPRPgCh", "PitchPollRightProgramChange", 0, 127, 7)   --activate right-input pitch-poll
+params:add_number("PtchPRzPgCh", "PitchPollRezProgramChange", 0, 127, 8)    --apply pitch-poll(whichever is active) to pitch of rezonator
+for i=1,6 do                                                                                        --SoftCut Voices 1 thru 6
+  params:add_number("V"..i.."PreFreezePgCh", "V"..i.."PreFreezeProgramChange", 0, 127, (10*i)+29)  --freeze params per-voice
+  params:add_number("V"..i.."K2PgCh", "V"..i.."K2ProgramChange", 0, 127, (10*i)+30)                --
   params:add_number("V"..i.."K3PgCh", "V"..i.."K3ProgramChange", 0, 127, (10*i)+31)
   params:add_number("V"..i.."LFOPgCh", "V"..i.."LFOProgramChange", 0, 127, (10*i)+32)
   params:add_number("V"..i.."Amp2LnPgCh", "V"..i.."Amp2LengthProgramChange", 0, 127, (10*i)+33)
@@ -93,20 +92,18 @@ params:bang()   --other params exist in other files, but are instantiated first,
 
 --MIDI stuff
 
-m=midi.connect()
 m.event = function(data)
   local d = midi.to_msg(data)
   if d.type == "program_change" then
     if lrn>0 then            --MIDI-learn of program change
       if page>0 then
         if sel==25 then if lrn==2 then params:set("SPrePgChU",d.val) else params:set("SPrePgChD",d.val) end
-        elseif sel==24 then params:set("UIGoPgCh",d.val)      --while seq page number is selected
         elseif sel==3 then params:set("RestartPgCh",d.val)    --while file directory field is selected
         elseif sel==2 then params:set("FXVPgCh",d.val)
-        elseif sel==1 then params:set("PlayPgCh",d.val)       --while tempo is selected
-        elseif sel==0 then params:set("InputPgCh",d.val)      --while swing is selected
-        elseif sel==-1 then params:set("DelRevPgCh",d.val)    --while midi-learn is selected
-        elseif sel==-2 then params:set("FreezePgCh",d.val)
+        elseif sel==1 then params:set("PlayPgCh",d.val)       --while tempo is selected, midilearn this to 'play' button
+        elseif sel==0 then params:set("InputPgCh",d.val)      --while swing is selected... ='input monitor'
+        elseif sel==-1 then params:set("DelRevPgCh",d.val)    
+        elseif sel==-2 then params:set("FreezePgCh",d.val)    --
         elseif sel==-3 then params:set("PtchPLPgCh",d.val)
         elseif sel==-4 then params:set("PtchPRPgCh",d.val)
         elseif sel==-5 then params:set("PtchPRzPgCh",d.val)
@@ -130,7 +127,7 @@ m.event = function(data)
       end
     else                     --ProgramChange assignments/functions
       for i=1,6 do  --softcut page (used more often, so checking these 1st might be slightly faster)
-        if d.val == params:get("V"..i.."PreFreezePgCh") then pfreez[i]=1-pfreez[i]
+        if d.val == params:get("V"..i.."PreFreezePgCh") then voices[i].pfreez=1-voices[i].pfreez
         elseif d.val == params:get("V"..i.."K2PgCh") then
           if params:get("V"..i.."_Mod")==1 then              --Mode 1 = stutter(k2 turns stutter on/off)
             params:set("V"..i.."_Go",1-params:get("V"..i.."_Go"))  --clock turns rec off after 2-4beats
@@ -138,18 +135,19 @@ m.event = function(data)
           elseif params:get("V"..i.."_Mod")==2 then                          --Mode 2 = Delay
             params:set("V"..i.."_Go",1-params:get("V"..i.."_Go"))   --k2 turns delay on/off
           else      --Mode 3 = Live Looper; k2 sets recording to start at beginning of next cycle...
-            prerecz[i]=2                     --..then stop-recording and start-play at cycle after that
+            voice[i].prerec=2                     --..then stop-recording and start-play at cycle after that
           end
         elseif d.val == params:get("V"..i.."K3PgCh") then
-          if params:get("V"..i.."_Mod")==3 then lplay[i]=1-lplay[i] softcut.play(i,lplay[i])
+          if params:get("V"..i.."_Mod")==3 then voice[i].looplay=1-voice[i].looplay softcut.play(i,voice[i].looplay)
            else params:set("V"..i.."_Go",1-params:get("V"..i.."_Go")) end
         elseif d.val == params:get("V"..i.."LFOPgCh") then
-          poslfoz[i]:set('max', util.round(lasknwnpos[i]/58.0,0.0001))
-          plf[i]=util.wrap(plf[i]+1,0,3) poslfoz[i]:set('depth', 1.0)
-          if plf[i] == 0 then poslfoz[i]:stop()
-          elseif plf[i] == 1 then poslfoz[i]:set('shape', 'random') lfprmult=0.25 poslfoz[i]:start()  
-          elseif plf[i] == 2 then poslfoz[i]:set('shape', 'saw') lfprmult=0.75 poslfoz[i]:start()
-          elseif plf[i] == 3 then poslfoz[i]:set('shape', 'sine') lfprmult=0.5 poslfoz[i]:start() end
+          poslfoz[i]:set('max', util.round((voices[i].ennd[voices[i].lpno]%58)/58.0,0.0001))
+          poslfoz[i]:set('min', util.round((voices[i].strt[voices[i].lpno]%58)/58.0,0.0001))
+          voices[i].plf=util.wrap(voices[i].plf+1,0,3) poslfoz[i]:set('depth', 1.0)  --resetting 'depth' may no longer be necessary
+          if voices[i].plf == 0 then poslfoz[i]:stop()
+          elseif voices[i].plf == 1 then poslfoz[i]:set('shape', 'random') lfprmult=0.25 poslfoz[i]:start()  
+          elseif voices[i].plf == 2 then poslfoz[i]:set('shape', 'saw') lfprmult=0.75 poslfoz[i]:start()
+          elseif voices[i].plf == 3 then poslfoz[i]:set('shape', 'sine') lfprmult=0.5 poslfoz[i]:start() end
         elseif d.val == params:get("V"..i.."Amp2LnPgCh") then
           params:set("V"..i.."_ALn",1-params:get("V"..i.."_ALn"))
         elseif d.val == params:get("V"..i.."Ptch2LnPgCh") then
@@ -183,7 +181,6 @@ m.event = function(data)
       elseif d.val == params:get("VPreActivePgCh") then vpr=1-vpr if vpr>0 then params:set("VPre",vprenum) end
       elseif d.val == params:get("InputPgCh") then params:set("InMon", 1-params:get("InMon"))
       elseif d.val == params:get("FreezePgCh") then prmfreez=1-prmfreez
-      elseif d.val == params:get("UIGoPgCh") then uigo=1-uigo
       elseif d.val == params:get("RestartPgCh") then tix=0 tixx=-1
       elseif d.val == params:get("FXVPgCh") then params:set("Fxv", 1-params:get("Fxv"))
       elseif d.val == params:get("PlayPgCh") then
