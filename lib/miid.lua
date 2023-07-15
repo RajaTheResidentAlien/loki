d05e4203594de6349fce587b4e7a3d9c736593c6
Raja <45028516+RajaTheResidentAlien@users.noreply.github.com>
2023-07-14 17:57:33 -0700

    Update miid.lua

--- lib/miid.lua
+++ lib/miid.lua
@@ -14,6 +14,11 @@ function mpwrit(nam)                                 --Master preset write
   mwritab["RestartPgCh"]=params:get("RestartPgCh") mwritab["DelRevPgCh"]=params:get("DelRevPgCh") 
   mwritab["FXVPgCh"]=params:get("FXVPgCh") mwritab["PtchPLPgCh"]=params:get("PtchPLPgCh") 
   mwritab["PtchPRPgCh"]=params:get("PtchPRPgCh") mwritab["PtchPRzPgCh"]=params:get("PtchPRzPgCh")
+  for i=1,4 do
+    mwritab["S"..i.."_PlyPgCh"]=params:get("S"..i.."_PlyPgCh") mwritab["S"..i.."_SttPgCh"]=params:get("S"..i.."_SttPgCh")
+    mwritab["S"..i.."_RctPgCh"]=params:get("S"..i.."_RctPgCh") mwritab["S"..i.."_RlnPgCh"]=params:get("S"..i.."_RlnPgCh")
+    mwritab["S"..i.."_DflPgCh"]=params:get("S"..i.."_DflPgCh")
+  end
   for i=1,6 do
     mwritab["V"..i.."PreFreezePgCh"]=params:get("V"..i.."PreFreezePgCh") 
     mwritab["V"..i.."GoPgCh"]=params:get("V"..i.."GoPgCh")
@@ -23,8 +28,6 @@ function mpwrit(nam)                                 --Master preset write
     mwritab["V"..i.."Ptch2LnPgCh"]=params:get("V"..i.."Ptch2LnPgCh")
     mwritab["V"..i.."Amp2PosPgCh"]=params:get("V"..i.."Amp2PosPgCh")
     mwritab["V"..i.."Amp2SpdPgCh"]=params:get("V"..i.."Amp2SpdPgCh")
-    mwritab["V"..i.."AmpLOnPgCh"]=params:get("V"..i.."AmpLOnPgCh")
-    mwritab["V"..i.."AmpROnPgCh"]=params:get("V"..i.."AmpROnPgCh")
     mwritab["V"..i.."AmpRecPgCh"]=params:get("V"..i.."AmpRecPgCh")
   end
   tab.save(mwritab,_path.data.."loki/"..nam)
@@ -42,6 +45,13 @@ function mpread(nam)                                  --Master preset read
       params:set("RestartPgCh", mreadtab["RestartPgCh"]) params:set("DelRevPgCh", mreadtab["DelRevPgCh"])
       params:set("FXVPgCh", mreadtab["FXVPgCh"]) params:set("PtchPLPgCh", mreadtab["PtchPLPgCh"])
       params:set("PtchPRPgCh", mreadtab["PtchPRPgCh"]) params:set("PtchPRzPgCh", mreadtab["PtchPRzPgCh"])
+  
+    for i=1,4 do
+      params:set("S"..i.."_PlyPgCh",mreadtab["S"..i.."_PlyPgCh"]) params:set("S"..i.."_SttPgCh",mreadtab["S"..i.."_SttPgCh"])
+      params:set("S"..i.."_RctPgCh",mreadtab["S"..i.."_RctPgCh"]) params:set("S"..i.."_RlnPgCh",mreadtab["S"..i.."_RlnPgCh"])
+      params:set("S"..i.."_DflPgCh",mreadtab["S"..i.."_DflPgCh"])
+    end
+    
     for i=1,6 do 
       params:set("V"..i.."PreFreezePgCh", mreadtab["V"..i.."PreFreezePgCh"]) 
       params:set("V"..i.."RecPgCh", mreadtab["V"..i.."RecPgCh"])
@@ -51,8 +61,6 @@ function mpread(nam)                                  --Master preset read
       params:set("V"..i.."Ptch2LnPgCh", mreadtab["V"..i.."Ptch2LnPgCh"])
       params:set("V"..i.."Amp2PosPgCh", mreadtab["V"..i.."Amp2PosPgCh"])
       params:set("V"..i.."Amp2SpdPgCh", mreadtab["V"..i.."Amp2SpdPgCh"])
-      params:set("V"..i.."AmpLOnPgCh", mreadtab["V"..i.."AmpLOnPgCh"])
-      params:set("V"..i.."AmpROnPgCh", mreadtab["V"..i.."AmpROnPgCh"])
       params:set("V"..i.."AmpRecPgCh", mreadtab["V"..i.."AmpRecPgCh"])
     end
   end
@@ -67,36 +75,46 @@ params:add_number("SPre", "SPreset",1,500,1)
 params:set_action("SPre", function(spre) spread("S_"..spre..".lki") end) --    ""
 params:add_number("VPre", "VPreset",1,500,1)
 params:set_action("VPre", function(vpre) vpread("V_"..vpre..".lki") end) --    ""
-params:add_group("PgCh_Grp","ProgramChange_Group",81)  --
-params:add_number("SPrePgChD", "SPreProgramChangeDown", 0, 127, 10) -- S-Page program change down
-params:add_number("SPrePgChU", "SPreProgramChangeUp", 0, 127, 11)  -- S-Page program change up
-params:add_number("SPreActivePgCh", "SPreActiveProgramChange", 0, 127, 15)  --make S-page program changes active
-params:add_number("VPrePgChD", "VPreProgramChangeDown", 0, 127, 20)  -- etc...
-params:add_number("VPrePgChU", "VPreProgramChangeUp", 0, 127, 21)
-params:add_number("VPreActivePgCh", "VPreActiveProgramChange", 0, 127, 17)  
-params:add_number("InputPgCh", "InputMonitorProgramChange", 0, 127, 18)     --input-monitor toggle
-params:add_number("FreezePgCh", "FreezeProgramChange", 0, 127, 19)          --freeze tempo-related params(S-Page)
-params:add_number("TransprtPgCh", "TransportrogramChange", 0, 127, 14)              --transport play on/off
-params:add_number("RestartPgCh", "RestartProgramChange", 0, 127, 30)        --restart transport(w/out off)
-params:add_number("DelRevPgCh", "DelayReverbProgramChange", 0, 127, 4)      --toggle random delay/reverb
-params:add_number("FXVPgCh", "FXVortexProgramChange", 0, 127, 5)            --toggle FX Vortex on/off
-params:add_number("PtchPLPgCh", "PitchPollLeftProgramChange", 0, 127, 6)    --activate left-input pitch-poll
-params:add_number("PtchPRPgCh", "PitchPollRightProgramChange", 0, 127, 7)   --activate right-input pitch-poll
-params:add_number("PtchPRzPgCh", "PitchPollRezProgramChange", 0, 127, 8)  --apply pitch-poll(whichever's active) to rezon8r pitch
-for i=1,6 do                                                                                        --SoftCut Voices 1 thru 6
-  params:add_number("V"..i.."PreFreezePgCh", "V"..i.."PreFreezeProgramChange", 0, 127, (10*i)+29)  --freeze params per-voice
-  params:add_number("V"..i.."RecPgCh", "V"..i.."RecProgramChange", 0, 127, (10*i)+30)                --
-  params:add_number("V"..i.."GoPgCh", "V"..i.."GoProgramChange", 0, 127, (10*i)+31)
-  params:add_number("V"..i.."LFOPgCh", "V"..i.."LFOProgramChange", 0, 127, (10*i)+32)
-  params:add_number("V"..i.."Amp2LnPgCh", "V"..i.."Amp2LengthProgramChange", 0, 127, (10*i)+33)
-  params:add_number("V"..i.."Ptch2LnPgCh", "V"..i.."Pitch2LengthProgramChange", 0, 127, (10*i)+34)
-  params:add_number("V"..i.."Amp2PosPgCh", "V"..i.."Amp2PositionProgramChange", 0, 127, (10*i)+35)
-  params:add_number("V"..i.."Amp2SpdPgCh", "V"..i.."Amp2SpeedProgramChange", 0, 127, (10*i)+36)
-  params:add_number("V"..i.."AmpLOnPgCh", "V"..i.."AmpPollLeftNoffProgramChange", 0, 127, (10*i)+37)
-  params:add_number("V"..i.."AmpROnPgCh", "V"..i.."AmpPollRightNoffProgramChange", 0, 127, (10*i)+38)
-  params:add_number("V"..i.."AmpRecPgCh", "V"..i.."Amp2RecProgramChange", 0, 127, (10*i)+39)
+params:add_group("PgCh_Grp","MIDIProgChAssign_Group",103)  --
+params:add_number("MPrePgChD", "MPrePrgChDown", 0, 127, 12) -- Master-pre program change up
+params:add_number("MPrePgChU", "MPrePrgChUp", 0, 127, 13) -- Master-pre program change up
+params:add_number("SPrePgChD", "SPrePrgChDown", 0, 127, 10) -- S-Pag-pre program change down
+params:add_number("SPrePgChU", "SPrePrgChUp", 0, 127, 11)  -- S-Page-pre program change up
+params:add_number("SPreActivePgCh", "SPreActivePrgCh", 0, 127, 15)  --make S-page program changes active
+params:add_number("VPrePgChD", "VPrePrgChDown", 0, 127, 20)  -- etc...
+params:add_number("VPrePgChU", "VPrePrgChUp", 0, 127, 21)
+params:add_number("VPreActivePgCh", "VPreActivePrgCh", 0, 127, 17)  
+params:add_number("InputPgCh", "InputMonitorPrgCh", 0, 127, 18)     --input-monitor toggle
+params:add_number("FreezePgCh", "FreezePrgCh", 0, 127, 19)          --freeze tempo-related params(S-Page)
+params:add_number("TransprtPgCh", "TransportPrgCh", 0, 127, 14)              --transport play on/off
+params:add_number("RestartPgCh", "RestartPrgCh", 0, 127, 30)        --restart transport(w/out off)
+params:add_number("DelRevPgCh", "DelayReverbPrgCh", 0, 127, 4)      --toggle random delay/reverb
+params:add_number("FXVPgCh", "FXVortexPrgCh", 0, 127, 5)            --toggle FX Vortex on/off
+params:add_number("PtchPLPgCh", "PitchPollLeftPrgCh", 0, 127, 6)    --activate left-input pitch-poll
+params:add_number("PtchPRPgCh", "PitchPollRightPrgCh", 0, 127, 7)   --activate right-input pitch-poll
+params:add_number("PtchPRzPgCh", "PitchPollRezPrgCh", 0, 127, 8) --apply pitch-poll(whichever's active) to rezon8r pitch
+
+for i=1,4 do
+  params:add_number("S"..i.."_PlyPgCh", "S"..i.."_PlayPrgCh",0,127,22+i) 
+  params:add_number("S"..i.."_SttPgCh", "S"..i.."_StutterPrgCh",0,127,32+i)
+  params:add_number("S"..i.."_RctPgCh", "S"..i.."_RandomCutoffPrgCh",0,127,104+i) 
+  params:add_number("S"..i.."_RlnPgCh", "S"..i.."_RandomLengthPrgCh",0,127,109+i)
+  params:add_number("S"..i.."_DflPgCh", "S"..i.."_DrunkFileWalkPrgCh",0,127,114+i)
 end
-params:bang()   --other params exist in other files, but are instantiated first, bang here at the end
+
+for i=1,6 do                                                                                        --SoftCut Voices 1 thru 6
+  params:add_number("V"..i.."PreFreezePgCh", "V"..i.."PreFreezePrgCh", 0, 127, (10*i)+29)  --freeze params per-voice
+  params:add_number("V"..i.."RecPgCh", "V"..i.."RecPrgCh", 0, 127, (10*i)+30)                --
+  params:add_number("V"..i.."GoPgCh", "V"..i.."GoPrgCh", 0, 127, (10*i)+31)
+  params:add_number("V"..i.."LFOPgCh", "V"..i.."LFOPrgCh", 0, 127, (10*i)+32)
+  params:add_number("V"..i.."Amp2LnPgCh", "V"..i.."Amp2LengthPrgCh", 0, 127, (10*i)+33)     --applying polls...
+  params:add_number("V"..i.."Ptch2LnPgCh", "V"..i.."Pitch2LengthPrgCh", 0, 127, (10*i)+34)
+  params:add_number("V"..i.."Amp2PosPgCh", "V"..i.."Amp2PositionPrgCh", 0, 127, (10*i)+35)
+  params:add_number("V"..i.."Amp2SpdPgCh", "V"..i.."Amp2SpeedPrgCh", 0, 127, (10*i)+36)
+  params:add_number("V"..i.."AmpRecPgCh", "V"..i.."Amp2RecPrgCh", 0, 127, (10*i)+39)
+end
+
+params:bang() --other params exist in other files, but are instantiated first, bang here at the end
 
 --MIDI stuff
 
@@ -105,32 +123,37 @@ m.event = function(data)
   if d.type == "program_change" then
     if lrn>0 then            --MIDI-learn of program change
       if page==1 then
-        if sel==25 then if lrn==2 then params:set("SPrePgChU",d.val) else params:set("SPrePgChD",d.val) end
-        elseif sel==3 then params:set("RestartPgCh",d.val)    --while file directory field is selected
+        if sel==25 then if lrn==2 then params:set("MPrePgChU",d.val) else params:set("MPrePgChD",d.val) end
+        elseif sel==24 then if lrn==2 then params:set("SPrePgChU",d.val) else params:set("SPrePgChD",d.val) end
+        elseif sel==-7 then params:set("SPreActivePgCh",1-params:get("SPreActivePgCh")) --while 'thresh:' is selected
+        elseif sel==0 then params:set("RestartPgCh",d.val)  --while tempo is selected, map to transport-restart(no stop)
         elseif sel==2 then params:set("FXVPgCh",d.val)
-        elseif sel==1 then params:set("PlayPgCh",d.val)       --while tempo is selected, midilearn this to 'play' button
-        elseif sel==0 then params:set("InputPgCh",d.val)      --while swing is selected... ='input monitor'
-        elseif sel==-1 then params:set("DelRevPgCh",d.val)    
-        elseif sel==-2 then params:set("FreezePgCh",d.val)    --
-        elseif sel==-3 then params:set("PtchPLPgCh",d.val)
-        elseif sel==-4 then params:set("PtchPRPgCh",d.val)
-        elseif sel==-5 then params:set("PtchPRzPgCh",d.val)
+        elseif sel==-1 then params:set("TransprtPgCh",d.val) --(regular transport start/stop)
+        elseif sel==-2 then params:set("InputPgCh",d.val)    --while midilearn is selected... = map to 'input monitor'
+        elseif sel==1 then params:set("DelRevPgCh",d.val)    --while swing is selected..map to delay/reverb toggle
+        elseif sel==-3 then params:set("FreezePgCh",d.val)
+        elseif sel==-6 then params:set("PtchPLPgCh",d.val)
+        elseif sel==-5 then params:set("PtchPRPgCh",d.val)
+        elseif sel==-4 then params:set("PtchPRzPgCh",d.val)
+        elseif sel==-9 then params:set("AT1PgCh",d.val)
+        elseif sel==-8 then params:set("AT2PgCh",d.val)
+        elseif ((sel>3) and (sel<8)) then params:set("S"..(sel-3).."_PlyPgCh",d.val)
+        elseif ((sel>7) and (sel<12)) then params:set("S"..(sel-7).."_DflPgCh",d.val)
+        elseif ((sel>11) and (sel<16)) then params:set("S"..(sel-11).."_RlnPgCh",d.val)
+        elseif ((sel>15) and (sel<20)) then params:set("S"..(sel-15).."_SttPgCh",d.val)
         end
       elseif page==3 then
-        if vsel==0 then if lrn==2 then params:set("VPrePgChU",d.val) else params:set("VPrePgChD",d.val) end 
-        else
-          if hsel==-1 then params:set("V"..vsel.."PreFreezePgCh",d.val) 
-          elseif hsel==0 then params:set("V"..vsel.."RecPgCh",d.val) 
-          elseif hsel==1 then params:set("V"..vsel.."GoPgCh",d.val) 
-          elseif hsel==2 then params:set("V"..vsel.."LFOPgCh",d.val)
-          elseif hsel==5 then params:set("V"..vsel.."Amp2LnPgCh",d.val)
-          elseif hsel==7 then params:set("V"..vsel.."Ptch2LnPgCh",d.val)
-          elseif hsel==8 then params:set("V"..vsel.."Amp2PosPgCh",d.val)
-          elseif hsel==10 then params:set("V"..vsel.."Amp2SpdPgCh",d.val)
-          elseif hsel==15 then params:set("V"..vsel.."AmpLOnPgCh",d.val)
-          elseif hsel==16 then params:set("V"..vsel.."AmpROnPgCh",d.val)
-          elseif hsel==17 then params:set("V"..vsel.."AmpRecPgCh",d.val)
-          end
+        if hsel==-1 then if lrn==2 then params:set("VPrePgChU",d.val) else params:set("VPrePgChD",d.val) end
+        elseif hsel==5 then params:set("VPreActivePgCh",d.val) --while mode is selected
+        elseif hsel==2 then params:set("V"..vsel.."PreFreezePgCh",d.val) 
+        elseif hsel==3 then params:set("V"..vsel.."RecPgCh",d.val) --select the voice-playback UI toggle(just right of voice#)
+        elseif hsel==0 then params:set("V"..vsel.."GoPgCh",d.val)   --select the transport button
+        elseif hsel==16 then params:set("V"..vsel.."LFOPgCh",d.val)
+        elseif hsel==6 then params:set("V"..vsel.."Amp2LnPgCh",d.val)
+        elseif hsel==7 then params:set("V"..vsel.."Ptch2LnPgCh",d.val)
+        elseif hsel==9 then params:set("V"..vsel.."Amp2PosPgCh",d.val)
+        elseif hsel==11 then params:set("V"..vsel.."Amp2SpdPgCh",d.val)
+        elseif hsel==15 then params:set("V"..vsel.."AmpRecPgCh",d.val)
         end
       end
     else                     --ProgramChange assignments/functions
@@ -162,16 +185,26 @@ m.event = function(data)
           params:set("V"..i.."_APs",1-params:get("V"..i.."_APs"))
         elseif d.val == params:get("V"..i.."Amp2SpdPgCh") then
           params:set("V"..i.."_ASp",1-params:get("V"..i.."_ASp"))
-        elseif d.val == params:get("V"..i.."AmpLOnPgCh") then
-          params:set("V"..i.."_AT1",1-params:get("V"..i.."_AT1"))
-        elseif d.val == params:get("V"..i.."AmpROnPgCh") then
-          params:set("V"..i.."_AT2",1-params:get("V"..i.."_AT2"))
         elseif d.val == params:get("V"..i.."AmpRecPgCh") then
           params:set("V"..i.."_ARc",1-params:get("V"..i.."_ARc"))
         end
       end
-                                                          --main page
-      if d.val == params:get("SPrePgChD") then
+                                              --main page--
+      for i=1,4 do
+        if d.val==params:get("S"..i.."_PlyPgCh") then params:set("S"..i.."_Ply",1-params:get("S"..i.."_Ply"))
+        elseif d.val==params:get("S"..i.."_SttPgCh") then params:set("S"..i.."_Stt",1-params:get("S"..i.."_Stt"))
+        elseif d.val==params:get("S"..i.."_RctPgCh") then params:set("S"..i.."_Rct",1-params:get("S"..i.."_Rct"))
+        elseif d.val==params:get("S"..i.."_RlnPgCh") then params:set("S"..i.."_Rln",1-params:get("S"..i.."_Rln"))
+        elseif d.val==params:get("S"..i.."_DflPgCh") then params:set("S"..i.."_Dfl",1-params:get("S"..i.."_Dfl")) end
+      end
+                                                          
+      if d.val == params:get("MPrePgChD") then
+        --if spr>0 then params:set("MPre", util.wrap(params:get("MPre")-1,1,500)) sprenum = params:get("MPre")
+          --else sprenum = util.wrap(sprenum-1,1,500) end
+      elseif d.val == params:get("SPrePgChU") then
+        if spr>0 then params:set("SPre", util.wrap(params:get("SPre")+1,1,500)) sprenum = params:get("SPre")
+          else sprenum = util.wrap(sprenum+1,1,500) end
+      elseif d.val == params:get("SPrePgChD") then
         if spr>0 then params:set("SPre", util.wrap(params:get("SPre")-1,1,500)) sprenum = params:get("SPre")
           else sprenum = util.wrap(sprenum-1,1,500) end
       elseif d.val == params:get("SPrePgChU") then
@@ -196,6 +229,7 @@ m.event = function(data)
       elseif d.val == params:get("PtchPRPgCh") then params:set("PT2",1-params:get("PT2")) 
       elseif d.val == params:get("PtchPRzPgCh") then params:set("S_PRz",1-params:get("S_PRz"))
       end
+      if(rdrw<1) then rdrw=1 end
     end
   end
 end
