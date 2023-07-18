73a797097930a3fac8d725c37ee3b6f6c1ad84b7
Raja <45028516+RajaTheResidentAlien@users.noreply.github.com>
2023-07-18 08:55:22 -0700

    Update miid.lua

--- lib/miid.lua
+++ lib/miid.lua
@@ -15,17 +15,17 @@ function mpwrit(nam)                                 --Master preset write
   mwritab["FXVPgCh"]=params:get("FXVPgCh") mwritab["PtchPLPgCh"]=params:get("PtchPLPgCh") 
   mwritab["PtchPRPgCh"]=params:get("PtchPRPgCh") mwritab["PtchPRzPgCh"]=params:get("PtchPRzPgCh")
   for i=1,4 do
-    mwritab["S"..i.."_PlyPgCh"]=params:get("S"..i.."_PlyPgCh") mwritab["S"..i.."_SttPgCh"]=params:get("S"..i.."_SttPgCh")
-    mwritab["S"..i.."_RctPgCh"]=params:get("S"..i.."_RctPgCh") mwritab["S"..i.."_RlnPgCh"]=params:get("S"..i.."_RlnPgCh")
+    mwritab["S"..i.."_PlyPgCh"]=params:get("S"..i.."_PlyPgCh") mwritab["S"..i.."_RlnPgCh"]=params:get("S"..i.."_RlnPgCh")
     mwritab["S"..i.."_DflPgCh"]=params:get("S"..i.."_DflPgCh")
   end
   for i=1,6 do
     mwritab["V"..i.."PreFreezePgCh"]=params:get("V"..i.."PreFreezePgCh") 
     mwritab["V"..i.."GoPgCh"]=params:get("V"..i.."GoPgCh")
     mwritab["V"..i.."RecPgCh"]=params:get("V"..i.."RecPgCh")
-    mwritab["V"..i.."LFOPgCh"]=params:get("V"..i.."LFOPgCh")
     mwritab["V"..i.."Amp2LnPgCh"]=params:get("V"..i.."Amp2LnPgCh")
     mwritab["V"..i.."Ptch2LnPgCh"]=params:get("V"..i.."Ptch2LnPgCh")
+    mwritab["V"..i.."_LpNumPgChU"]=params:get("V"..i.."_LpNumPgChU")
+    mwritab["V"..i.."_LpNumPgChD"]=params:get("V"..i.."_LpNumPgChD")
     mwritab["V"..i.."Amp2PosPgCh"]=params:get("V"..i.."Amp2PosPgCh")
     mwritab["V"..i.."Amp2SpdPgCh"]=params:get("V"..i.."Amp2SpdPgCh")
     mwritab["V"..i.."AmpRecPgCh"]=params:get("V"..i.."AmpRecPgCh")
@@ -47,8 +47,7 @@ function mpread(nam)                                  --Master preset read
       params:set("PtchPRPgCh", mreadtab["PtchPRPgCh"]) params:set("PtchPRzPgCh", mreadtab["PtchPRzPgCh"])
   
     for i=1,4 do
-      params:set("S"..i.."_PlyPgCh",mreadtab["S"..i.."_PlyPgCh"]) params:set("S"..i.."_SttPgCh",mreadtab["S"..i.."_SttPgCh"])
-      params:set("S"..i.."_RctPgCh",mreadtab["S"..i.."_RctPgCh"]) params:set("S"..i.."_RlnPgCh",mreadtab["S"..i.."_RlnPgCh"])
+      params:set("S"..i.."_PlyPgCh",mreadtab["S"..i.."_PlyPgCh"]) params:set("S"..i.."_RlnPgCh",mreadtab["S"..i.."_RlnPgCh"])
       params:set("S"..i.."_DflPgCh",mreadtab["S"..i.."_DflPgCh"])
     end
     
@@ -56,9 +55,10 @@ function mpread(nam)                                  --Master preset read
       params:set("V"..i.."PreFreezePgCh", mreadtab["V"..i.."PreFreezePgCh"]) 
       params:set("V"..i.."RecPgCh", mreadtab["V"..i.."RecPgCh"])
       params:set("V"..i.."GoPgCh", mreadtab["V"..i.."GoPgCh"])
-      params:set("V"..i.."LFOPgCh", mreadtab["V"..i.."LFOPgCh"])
       params:set("V"..i.."Amp2LnPgCh", mreadtab["V"..i.."Amp2LnPgCh"])
       params:set("V"..i.."Ptch2LnPgCh", mreadtab["V"..i.."Ptch2LnPgCh"])
+      params:set("V"..i.."_LpNumPgChU", mreadtab["V"..i.."_LpNumPgChU"])
+      params:set("V"..i.."_LpNumPgChD", mreadtab["V"..i.."_LpNumPgChD"])
       params:set("V"..i.."Amp2PosPgCh", mreadtab["V"..i.."Amp2PosPgCh"])
       params:set("V"..i.."Amp2SpdPgCh", mreadtab["V"..i.."Amp2SpdPgCh"])
       params:set("V"..i.."AmpRecPgCh", mreadtab["V"..i.."AmpRecPgCh"])
@@ -75,7 +75,7 @@ params:add_number("SPre", "SPreset",1,500,1)
 params:set_action("SPre", function(spre) spread("S_"..spre..".lki") end) --    ""
 params:add_number("VPre", "VPreset",1,500,1)
 params:set_action("VPre", function(vpre) vpread("V_"..vpre..".lki") end) --    ""
-params:add_group("PgCh_Grp","MIDIProgChAssign_Group",103)  --
+params:add_group("PgCh_Grp","MIDIProgChAssign_Group",77)  --
 params:add_number("MPrePgChD", "MPrePrgChDown", 0, 127, 12) -- Master-pre program change up
 params:add_number("MPrePgChU", "MPrePrgChUp", 0, 127, 13) -- Master-pre program change up
 params:add_number("SPrePgChD", "SPrePrgChDown", 0, 127, 10) -- S-Pag-pre program change down
@@ -96,8 +96,6 @@ params:add_number("PtchPRzPgCh", "PitchPollRezPrgCh", 0, 127, 8) --apply pitch-p
 
 for i=1,4 do
   params:add_number("S"..i.."_PlyPgCh", "S"..i.."_PlayPrgCh",0,127,22+i) 
-  params:add_number("S"..i.."_SttPgCh", "S"..i.."_StutterPrgCh",0,127,32+i)
-  params:add_number("S"..i.."_RctPgCh", "S"..i.."_RandomCutoffPrgCh",0,127,104+i) 
   params:add_number("S"..i.."_RlnPgCh", "S"..i.."_RandomLengthPrgCh",0,127,109+i)
   params:add_number("S"..i.."_DflPgCh", "S"..i.."_DrunkFileWalkPrgCh",0,127,114+i)
 end
@@ -106,9 +104,10 @@ for i=1,6 do                                                                    
   params:add_number("V"..i.."PreFreezePgCh", "V"..i.."PreFreezePrgCh", 0, 127, (10*i)+29)  --freeze params per-voice
   params:add_number("V"..i.."RecPgCh", "V"..i.."RecPrgCh", 0, 127, (10*i)+30)                --
   params:add_number("V"..i.."GoPgCh", "V"..i.."GoPrgCh", 0, 127, (10*i)+31)
-  params:add_number("V"..i.."LFOPgCh", "V"..i.."LFOPrgCh", 0, 127, (10*i)+32)
   params:add_number("V"..i.."Amp2LnPgCh", "V"..i.."Amp2LengthPrgCh", 0, 127, (10*i)+33)     --applying polls...
   params:add_number("V"..i.."Ptch2LnPgCh", "V"..i.."Pitch2LengthPrgCh", 0, 127, (10*i)+34)
+  params:add_number("V"..i.."_LpNumPgChU", "V"..i.."_LoopNumPrgChUp",0,127,(10*i)+37)
+  params:add_number("V"..i.."_LpNumPgChD", "V"..i.."_LoopNumPrgChDown",0,127,(10*i)+38)
   params:add_number("V"..i.."Amp2PosPgCh", "V"..i.."Amp2PositionPrgCh", 0, 127, (10*i)+35)
   params:add_number("V"..i.."Amp2SpdPgCh", "V"..i.."Amp2SpeedPrgCh", 0, 127, (10*i)+36)
   params:add_number("V"..i.."AmpRecPgCh", "V"..i.."Amp2RecPrgCh", 0, 127, (10*i)+39)
@@ -140,7 +139,6 @@ m.event = function(data)
         elseif ((sel>3) and (sel<8)) then params:set("S"..(sel-3).."_PlyPgCh",d.val)
         elseif ((sel>7) and (sel<12)) then params:set("S"..(sel-7).."_DflPgCh",d.val)
         elseif ((sel>11) and (sel<16)) then params:set("S"..(sel-11).."_RlnPgCh",d.val)
-        elseif ((sel>15) and (sel<20)) then params:set("S"..(sel-15).."_SttPgCh",d.val)
         end
       elseif page==3 then
         if hsel==-1 then if lrn==2 then params:set("VPrePgChU",d.val) else params:set("VPrePgChD",d.val) end
@@ -148,10 +146,12 @@ m.event = function(data)
         elseif hsel==2 then params:set("V"..vsel.."PreFreezePgCh",d.val) 
         elseif hsel==3 then params:set("V"..vsel.."RecPgCh",d.val) --select the voice-playback UI toggle(just right of voice#)
         elseif hsel==0 then params:set("V"..vsel.."GoPgCh",d.val)   --select the transport button
-        elseif hsel==16 then params:set("V"..vsel.."LFOPgCh",d.val)
         elseif hsel==6 then params:set("V"..vsel.."Amp2LnPgCh",d.val)
         elseif hsel==7 then params:set("V"..vsel.."Ptch2LnPgCh",d.val)
-        elseif hsel==9 then params:set("V"..vsel.."Amp2PosPgCh",d.val)
+        elseif hsel==9 then 
+          if params:get("V"..vsel.."_Mod")==3 then 
+            if lrn==2 then params:set("V"..vsel.."_LpNumPgChU",d.val) else params:set("V"..vsel.."_LpNumPgChD",d.val) end
+          else params:set("V"..vsel.."Amp2PosPgCh",d.val) end
         elseif hsel==11 then params:set("V"..vsel.."Amp2SpdPgCh",d.val)
         elseif hsel==15 then params:set("V"..vsel.."AmpRecPgCh",d.val)
         end
@@ -169,31 +169,18 @@ m.event = function(data)
             voice[i].prerec=2                     --..then stop-recording and start-play at cycle after that
           end
         elseif d.val == params:get("V"..i.."GoPgCh") then params:set("V"..i.."_Go",1-params:get("V"..i.."_Go"))
-        elseif d.val == params:get("V"..i.."LFOPgCh") then
-          poslfoz[i]:set('max', util.round((voices[i].ennd[voices[i].lpno]%58)/58.0,0.0001))
-          poslfoz[i]:set('min', util.round((voices[i].strt[voices[i].lpno]%58)/58.0,0.0001))
-          voices[i].plf=util.wrap(voices[i].plf+1,0,3) poslfoz[i]:set('depth', 1.0) --resetting 'depth' may no longer be necessary
-          if voices[i].plf == 0 then poslfoz[i]:stop()
-          elseif voices[i].plf == 1 then poslfoz[i]:set('shape', 'random') lfprmult=0.25 poslfoz[i]:start()  
-          elseif voices[i].plf == 2 then poslfoz[i]:set('shape', 'saw') lfprmult=0.75 poslfoz[i]:start()
-          elseif voices[i].plf == 3 then poslfoz[i]:set('shape', 'sine') lfprmult=0.5 poslfoz[i]:start() end
-        elseif d.val == params:get("V"..i.."Amp2LnPgCh") then
-          params:set("V"..i.."_ALn",1-params:get("V"..i.."_ALn"))
-        elseif d.val == params:get("V"..i.."Ptch2LnPgCh") then
-          params:set("V"..i.."_PLn",1-params:get("V"..i.."_PLn"))
-        elseif d.val == params:get("V"..i.."Amp2PosPgCh") then
-          params:set("V"..i.."_APs",1-params:get("V"..i.."_APs"))
-        elseif d.val == params:get("V"..i.."Amp2SpdPgCh") then
-          params:set("V"..i.."_ASp",1-params:get("V"..i.."_ASp"))
-        elseif d.val == params:get("V"..i.."AmpRecPgCh") then
-          params:set("V"..i.."_ARc",1-params:get("V"..i.."_ARc"))
+        elseif d.val == params:get("V"..i.."Amp2LnPgCh") then params:set("V"..i.."_ALn",1-params:get("V"..i.."_ALn"))
+        elseif d.val == params:get("V"..i.."Ptch2LnPgCh") then params:set("V"..i.."_PLn",1-params:get("V"..i.."_PLn"))
+        elseif d.val == params:get("V"..i.."_LpNumPgChU") then params:set("V"..i.."_LpNum",params:get("V"..i.."_LpNum")+1)
+        elseif d.val == params:get("V"..i.."_LpNumPgChD") then params:set("V"..i.."_LpNum",params:get("V"..i.."_LpNum")-1)
+        elseif d.val == params:get("V"..i.."Amp2PosPgCh") then params:set("V"..i.."_APs",1-params:get("V"..i.."_APs"))
+        elseif d.val == params:get("V"..i.."Amp2SpdPgCh") then params:set("V"..i.."_ASp",1-params:get("V"..i.."_ASp"))
+        elseif d.val == params:get("V"..i.."AmpRecPgCh") then params:set("V"..i.."_ARc",1-params:get("V"..i.."_ARc"))
         end
       end
                                               --main page--
       for i=1,4 do
         if d.val==params:get("S"..i.."_PlyPgCh") then params:set("S"..i.."_Ply",1-params:get("S"..i.."_Ply"))
-        elseif d.val==params:get("S"..i.."_SttPgCh") then params:set("S"..i.."_Stt",1-params:get("S"..i.."_Stt"))
-        elseif d.val==params:get("S"..i.."_RctPgCh") then params:set("S"..i.."_Rct",1-params:get("S"..i.."_Rct"))
         elseif d.val==params:get("S"..i.."_RlnPgCh") then params:set("S"..i.."_Rln",1-params:get("S"..i.."_Rln"))
         elseif d.val==params:get("S"..i.."_DflPgCh") then params:set("S"..i.."_Dfl",1-params:get("S"..i.."_Dfl")) end
       end
